7ec6022689b36ad76e05e5f2eb3dc7597944cae3
dhcrain <dhcrain@gmail.com>
2016-06-01 16:52:47 -0400

    Normal mode is complete

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import datetime
 
 
 def welcome():
-    print("Welcome to HAL 9000")
-    hello = input("Would you like to login? Y/n ").lower()
+    hello = input("\nWould you like to login? Y/n ").lower()
     if hello != "n":
         login()
     else:
@@ -24,7 +23,7 @@ def new_user():
     username = input("Enter a new username: ")
 
     with open("user_info.csv") as infile:
-        user_data = csv.DictReader(infile, fieldnames=["username", "password", "full_name", "date_created"])
+        user_data = csv.DictReader(infile, fieldnames=["username", "password", "full_name", "fav_color", "date_created"])
         for user in user_data:
             if username.lower() == user["username"].lower():
                 print("That is not a valid username, try again.")
@@ -48,24 +47,23 @@ def new_user():
 
 def login():
     login_user_name = input("Username: ")
+    login_password = input("Password: ")
 
-    # read file
     with open("user_info.csv") as infile:
-        user_data = csv.DictReader(infile, fieldnames=[
-            "username", "password", "full_name", "fav_color", "date_created"])
-
+        user_data = csv.DictReader(infile, fieldnames=["username", "password", "full_name", "fav_color", "date_created"])
+        # for user in user_data:
+        #     print(user["username"])
         for user in user_data:
-            if login_user_name.lower() == user["username"].lower():
-                login_password = input("Password: ")
+            if login_user_name == user["username"]:
                 if login_password == user["password"]:
                     print(user)
                     logout_or_create()
                 else:
                     print("1 * Not a valid entry *")
                     welcome()
-            else:
-                print("2 * Not a valid entry *")
-                welcome()
+        else:
+            print("2 * Not a valid entry *")
+            welcome()
 
 
 def logout_or_create():
